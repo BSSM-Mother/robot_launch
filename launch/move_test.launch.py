810bafeb615a7300100f5148ecdb74f51df965f0
bitbyte08 <me@bitworkspace.kr>
2026-03-20 08:14:35 +0900

    fix :: 파라미터 수정

--- launch/move_test.launch.py
+++ launch/move_test.launch.py
@@ -12,6 +12,8 @@ def generate_launch_description():
             output='screen',
             parameters=[
                 {'use_sim_time': False},
+                {'wheel_distance': 0.07},   # 휠 간격 70mm
+                {'min_pwm': 100},           # 최소 PWM — 모터가 생각보다 강함
             ]
         ),
     ])
